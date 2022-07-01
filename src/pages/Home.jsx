304b765b2590c60b1d57d47189405e60de4b71b8
josemanuelbriones <josembrionesher@gmail.com>
2022-06-30 21:39:46 -0500

    primer version 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,8 @@
-
 import React, { useState, useEffect } from 'react';
 import {Leer} from '../api'
 import '@styles/tarjeta.scss';
 import aquivectores from '../assets/img/aqui-vextor-ok.png';
-
+import personafoto from '../assets/personas/persona1.jpg';
 const Home = () => {
 	const [persona, mostrar]= useState(null);
 
@@ -36,7 +35,7 @@ LeerPersona();
 					<label className="labelindica"><p>Ubicacion:</p>{p.data().ubicacion}</label>
 					<label className="labelindica"><p>Detalles:</p>{p.data().detalles}</label>
 					<label className="labelindica"><p>Genero:</p>{p.data().genero}</label>
-					<a className='botonF' href="/">Mas informacion</a>
+					<button type="button" className="btn btn-primary btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@getbootstrap">Datos</button>
 					{/* <label className="labelindica">{p.data().complexion}</label>
 					<label className="labelindica">{p.data().cabello}</label>
 					<label className="labelindica">{p.data().ojos}</label>
@@ -46,8 +45,46 @@ LeerPersona();
 					<label className="labelindica">{p.data().otrasfotos}</label> */}
 				</section>
 			  </div>
+			  
 			)
 		}
+		  <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div className="modal-dialog">
+                                        <div className="modal-content">
+                                        <div className="modal-header">
+                                            <h5 className="modal-title" id="exampleModalLabel">Localizada</h5>
+                                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div className="modal-body">
+                                            <div className="continer">
+                                                <div className="row d-flex justify-content-center aling-items-center">
+													<div className="col d-flex justify-content-center aling-items-center">
+													<img src={personafoto} className="img-fluid" alt="..." />
+													</div>
+													<div className="col">
+
+													
+                                                  <p className="card-text parrafo12">Fecha: 12/07/2022</p>
+                                                  <p className="card-text parrafo12">Ubicación: Metro Pino Suarez </p>
+                                                  <p className="card-text parrafo12">Detalles: no hay</p>
+                                                  <p className="card-text parrafo12">Género: mujer </p>
+                                                  <p className="card-text parrafo12">Complexión: morena </p>
+                                                  <p className="card-text parrafo12">Cabello: blanco </p>
+                                                  <p className="card-text parrafo12">Ojos: obscuros</p>
+                                                  <p className="card-text parrafo12">Tez: morena </p>
+                                                  <p className="card-text parrafo12">Altura: 170 cm</p>
+                                                  
+													</div>
+                                                  
+                                                </div>
+                                              </div>
+                                        </div>
+                                        <div className="modal-footer">
+                                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Contactar</button>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
 		</div>
 	</div>
 	);
--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -9,7 +9,7 @@ const Registro = () => {
     const [cabello, setCabello]= useState(null);
     const [ojos, setOjos]= useState(null);
     const [tez, setTez]= useState(null);
-    const [comentario, setComentario]= useState(null);
+    const [detalles, setDetalles]= useState(null);
     const [genero, setGenero]= useState(null);
     const [ubicacion, setUbicacion]= useState(null);
     const [fecha, setFecha] = useState(null)
@@ -19,7 +19,7 @@ const Registro = () => {
         ojos,
         tez,
         altura,
-        comentario,
+        detalles,
         genero,
         ubicacion,
         fecha
@@ -32,21 +32,30 @@ const Registro = () => {
         <form id="formulario-producto">
             <fieldset>
                 <legend>Agrega a esa persona localizada</legend>
-                
+                <label htmlFor="fecha-registro">Fecha de registro</label>
                 <input type="date" className='form-control' id="fecha-registro" placeholder="Fecha de registro" value={fecha}  onChange={(e) => setFecha(e.target.value)}/>
+                <label htmlFor="ubicacion">Ubicacion</label>
                 <input type="text" className='form-control' id="ubicacion" placeholder="dónde lo encontraste" value={ubicacion}  onChange={(e) => setUbicacion(e.target.value)}/>                
+                <label htmlFor="cars">Sexo</label>
                 <select id="cars" name="cars" value={genero}  onChange={(e) => setGenero(e.target.value)}>
                     <option value="0">sexo</option>
                     <option value="Hombre">hombre</option>
                     <option value="Mujer">mujer</option>
                 </select>
+                <label htmlFor="complexion">Complexion</label>
                 <input type="text" className='form-control' id="complexion" placeholder="flaco/promedio/gordo" value={complexion}  onChange={(e) => setComplexion(e.target.value)}/>
+                <label htmlFor="cabello">Color de cabello</label>
                 <input type="text" className='form-control' id="cabello" placeholder="negro/ blanco/ castaño/ rubio" value={cabello}  onChange={(e) => setCabello(e.target.value)}/>
+                <label htmlFor="ojos">Color de ojos</label>
                 <input type="text" className='form-control' id="ojos" placeholder="obscuro/ claro" value={ojos}  onChange={(e) => setOjos(e.target.value)}/>
+                <label htmlFor="tez">Color de piel</label>
                 <input type="text" className='form-control' id="tez" placeholder="blanco moreno" value={tez}  onChange={(e) => setTez(e.target.value)}/>
+                <label htmlFor="altura">Altura</label>
                 <input type="number" className='form-control' id="altura" placeholder="altura" value={altura}  onChange={(e) => setAltura(e.target.value)} min="0" step="0.01" max="1000"/>
-                <input type="text" className='form-control' id="foto" placeholder="foto"/>
-                <input type="text" className='form-control' id="comentario" value={comentario}  onChange={(e) => setComentario(e.target.value)} placeholder="comentarios"/>
+                <label htmlFor="foto">Selecciones alguna foto de sujeto:</label>
+                <input type="file" className='form-control' id="foto" placeholder="foto"/>
+                <label htmlFor="detalle">Detalles del sujeto:</label>
+                <input type="text" className='form-control' id="detalle" value={detalles}  onChange={(e) => setDetalles(e.target.value)} placeholder="detalles"/>
                 <button type="button" className='btn btn-primary btn-lg' onClick={salvarPersona}>Enviar</button>
                 
             </fieldset>
